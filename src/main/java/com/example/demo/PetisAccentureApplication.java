e255f31779564f3f941a144523e27e1680e85425
Matheus Albuquerque <matheus.albuquerque@hotmail.com>
2021-11-20 16:56:15 -0300

    Relacionamento de classes

---
+++ src/main/java/com/example/demo/PetisAccentureApplication.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.domain.Animal;
+import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Funcionario;
+import com.example.demo.repositories.AnimalRepository;
+import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.repositories.FuncionarioRepository;
+
+@SpringBootApplication
+public class PetisAccentureApplication implements CommandLineRunner {
+	
+	@Autowired
+	private AnimalRepository animalRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PetisAccentureApplication.class, args);
+	}
+  
+	@Override
+	public void run(String... args) throws Exception {
+		
+	 Cliente cliente1 = new Cliente(null,"Matheus","Recife","70392391","matheus123@hotmail.com",
+			 "123456","99874239","BOLETO");
+	 clienteRepository.saveAll(Arrays.asList(cliente1));
+
+	 
+	 Animal animal1 = new Animal(null,"Fred",15,"Poodle","Cachorro",cliente1);
+		
+		animalRepository.saveAll(Arrays.asList(animal1));
+		
+		 
+	 
+	 
+  
+  
+ 
+		
+	}
+}
---
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -0,0 +1,171 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String endereco;
+	private String cpf;
+	private String email;
+	private String senha;
+	private String numero;
+	private String pagamento;
+	
+  
+	
+	@JsonManagedReference
+    @OneToMany(mappedBy="cliente")
+	private List<Animal> animais = new LinkedList<>();
+	
+	
+	public Cliente(Integer id, String nome, String endereco, String cpf, String email, String senha, String numero,String pagamento) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.numero = numero;
+		this.pagamento = pagamento;
+	}
+
+
+	public String getPagamento() {
+		return pagamento;
+	}
+
+
+	public void setPagamento(String pagamento) {
+		this.pagamento = pagamento;
+	}
+
+
+	public Cliente() {
+		
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+	
+
+	public List<Animal> getAnimais() {
+		return animais;
+	}
+
+
+	public void setAnimais(List<Animal> animais) {
+		this.animais = animais;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
